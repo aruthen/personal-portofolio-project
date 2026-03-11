3fe137189ffa5605b5ee71da18038e6563e2c57f
Marutha Wira Yuda <maruthawira@gmail.com>
2026-03-11 15:14:20 +0700

    Make Instagram images fit frame without zoom

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
             v-else
             :src="photo.thumbnailUrl"
             :alt="`Instagram post ${index + 1}`"
-            class="h-full w-full object-cover transition duration-500 group-hover:scale-105"
+            class="h-full w-full bg-black object-contain"
             loading="lazy"
           />
           <div class="absolute inset-0 bg-black/30 transition duration-300 group-hover:bg-black/10"></div>
